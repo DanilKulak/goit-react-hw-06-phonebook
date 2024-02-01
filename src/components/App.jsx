654358ab5e6753224f197bc4a06f313205aac488
DanilKulak <1998kylak@gmail.com>
2024-02-01 13:29:42 +0200

    new-commit-02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,6 @@
-import { useState } from 'react';
 import MyPhonebook from './MyPhonebook/PhonebookForm/MyPhonebook';
 
-
 const App = () => {
-
-const [contacts, setContacts] = useState([]);
-const [name, setName] = useState('');
 
 return (
   <>
